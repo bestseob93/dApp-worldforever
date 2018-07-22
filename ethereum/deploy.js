682bbaef0873648fd67b104dd3fbabe8f1f9073d
Hwan Seob Lee <bestseob93@gmail.com>
2018-07-22 21:06:00 +0900

    downgrade web3 version to .26

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
+const key = require('./env-key.json');
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 
 const compiledFactory = require('./build/CampaignFactory.json');
+const compiledHashStore = require('./build/HashStore.json');
 
 const provider = new HDWalletProvider(
-  process.env.MNEMONIC,
-  process.env.RINKEBY_URL
+  key.MNEMONIC,
+  key.RINKEBY_URL,
 );
 
 const web3 = new Web3(provider);
@@ -16,15 +17,24 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(
+  const deployFactory = await new web3.eth.Contract(
     JSON.parse(compiledFactory.interface))
-      .deploy({ data: '0x' + compiledFactory.bytecode })
-      .send({ gas: '1000000', from: accounts[0] })
-      .catch(err => {
-        if(err) console.log('error occured', err);
-      });
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] })
+    .catch(err => {
+      if (err) console.log('deploying factory error occured', err);
+    });
 
-  console.log('Contract deployed to ', result.options.address);
+  const deployHashStore = await new web3.eth.Contract(
+    JSON.parse(compiledHashStore.interface))
+    .deploy({ data: compiledHashStore.bytecode })
+    .send({ gas: '1000000', from: accounts[0] })
+    .catch(err => {
+      if (err) console.error('deploying hashstore error occured', err);
+    });
+
+  console.log('Factory Contract deployed to ', deployFactory);
+  console.log('HashStore Contract deployed to ', deployHashStore);
 };
 
 deploy();
